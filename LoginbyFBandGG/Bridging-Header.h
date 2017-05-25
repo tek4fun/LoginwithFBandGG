f6dc42d1a3ab0a1fff84e4318f046f6366814170
tek4fun <tudd4711@gmail.com>
2017-05-25 10:05:16 +0700

    first commit

---
+++ LoginbyFBandGG/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  LoginbyFBandGG
+//
+//  Created by Tu on 5/17/17.
+//  Copyright © 2017 MozaTech. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+
+#endif /* Bridging_Header_h */
---
+++ LoginbyFBandGG/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  LoginbyFBandGG
+//
+//  Created by Tu on 5/22/17.
+//  Copyright © 2017 MozaTech. All rights reserved.
+//
+
+import Foundation
